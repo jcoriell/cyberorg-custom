483c237798d35b34baa298277e3e6b9ee8c7f28f
jcoriell <47751211+jcoriell@users.noreply.github.com>
2021-03-18 16:57:43 -0500

    Update pxt.json, main.blocks, main.ts, pairing.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
 let myDictionary = dictionaries.createDictionary()
-dictionaries.setKeyValue(myDictionary, "key1", 5146)
-dictionaries.setKeyValue(myDictionary, "hello", "g")
-basic.showString("" + (dictionaries.getValue(myDictionary, "hello")))
+myDictionary.setKeyValueNumber("key1", 0)
+myDictionary.setKeyValueString("key1", "hello")
--- pairing.ts
+++ pairing.ts
@@ -1,29 +1,61 @@
 
-//% color=#0D9DDB weight=99 icon="\uf00b"
+//% color=#0D9DDB weight=99 icon="\uf02d"
 namespace dictionaries { 
-    
-    //% block = "new JS Object"
+ 
+    /**
+     * Creates a new dictionary-like object.
+    */
+    //% block = "new Dictionary"
     //% blockSetVariable=myDictionary
-    export function createDictionary(): {} {
-        let newDictionary: { [key: string]: any } = {};
+    export function createDictionary(): Dictionary {
+        let newDictionary = new Dictionary;
         return newDictionary;
     }
 
-    /**
-     * Set a key value pair for a dictionary
-     * @param dictionary is a dictionary
-     * @param key is a string, eg: "key1"
-     * @param value is the value at the key, eg: 6
-     */
-    //% block="%dictionary set key %key value %value"
-    export function setKeyValue(dictionary: any,  key: string, val: any ): void{
-        dictionary[key] = val;
-    }
 
-    //% block="%dictionary get value at key %key"
-    export function getValue(dictionary: any,  key: string ): any{
-        return dictionary[key];
-    }
+    let _dictionaries: Dictionary[];
+
+    export class Dictionary {
+
+        public structure: { [key: string]: any } = {};
+
+        constructor(){
+            init()
+            _dictionaries.push(this)
+        }
+
+        /**
+         * Set a key value pair for a dictionary
+         * @param dictionary is a dictionary
+         * @param key is a string, eg: "key1"
+         * @param value is the value at the key, eg: "hello"
+         */
+        //% block="in %myDictionary set key-value pair %key : %value"
+        setKeyValueString(key: string, val: string ): void{
+            this.structure[key] = val;
+        }
+
+        /**
+         * Set a key value pair for a dictionary
+         * @param dictionary is a dictionary
+         * @param key is a string, eg: "key1"
+         * @param value is the value at the key, eg: 6
+         */
+        //% block="in %myDictionary set key-value pair %key : %value"
+        setKeyValueNumber(key: string, val: number ): void{
+            this.structure[key] = val;
+        }
+
+        //% block="from %myDictionary get value at key %key"
+        getValue(key: string ): any{
+            return this.structure[key];
+        }
+    
+    }  
+
+    function init(): void {
+        _dictionaries = (<Dictionary[]>[]);
+    } 
 
 }
 
